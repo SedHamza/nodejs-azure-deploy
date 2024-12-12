4bb84072d4b1c07fa24623a98f3c8605be608720
SedHamza <sedhamza18@gmail.com>
2024-12-13 00:37:53 +0100

    last Commit

--- server.js
+++ server.js
@@ -18,21 +18,20 @@ app.get("/messages", (req, res) => {
     } else {
       res.status(200).json(results);
     }
-  });  
+  });
 });
 
 //pour recupere les message entre 2 person
 app.post("/messages", (req, res) => {
-  const { id, sender, message } = req.body;
+  const { sender, message } = req.body;
 
   if (!message || !sender) {
     return res
       .status(400)
       .json({ error: "Le contenu et l'expéditeur sont requis" });
   }
-
-  const sql = "INSERT INTO messages (id,sender, message) VALUES (?, ?,?)";
-  db.query(sql, [id, sender, message], (err, result) => {
+  const sql = "INSERT INTO message (sender, message) VALUES (?,?)";
+  db.query(sql, [sender, message], (err, result) => {
     if (err) {
       res.status(500).json({ error: "Erreur lors de l'ajout du message" });
     } else {
@@ -43,10 +42,9 @@ app.post("/messages", (req, res) => {
   });
 });
 
-
 app.get("/user/:username", (req, res) => {
   const username = req.params.username;
-  const sql = "SELECT * FROM users WHERE username = ?";
+  const sql = "SELECT * FROM user WHERE username = ?";
 
   db.query(sql, [username], (err, results) => {
     if (err) {
@@ -61,50 +59,20 @@ app.get("/user/:username", (req, res) => {
     }
   });
 });
-app.get("/users", (req, res) => {
-    const sql = "SELECT * FROM user";
-    db.query(sql, (err, results) => {
-      if (err) {
-        res
-          .status(500)
-          .json({ error: "Erreur lors de la récupération des messages" });
-      } else {
-        console.log(results);
-        res.status(200).json(results);
-      }
-    });
-  });
-  
 
 app.post("/user", (req, res) => {
   const { username, password } = req.body;
-  const checkSql = "SELECT * FROM users WHERE username = ?";
-
-  db.query(checkSql, [username], (err, results) => {
+  // Ajouter l'utilisateur
+  const insertSql = "INSERT INTO user (username, password) VALUES (?, ?)";
+  db.query(insertSql, [username, password], (err, result) => {
     if (err) {
       return res
         .status(500)
-        .json({ error: "Erreur lors de la vérification de l'utilisateur" });
+        .json({ error: "Erreur lors de l'ajout de l'utilisateur" });
     }
-
-    if (results.length > 0) {
-      return res.status(400).json({ error: "L'utilisateur existe déjà" });
-    }
-
-    // Ajouter l'utilisateur
-    const insertSql = "INSERT INTO users (username, password) VALUES (?, ?)";
-    db.query(insertSql, [username, password], (err, result) => {
-      if (err) {
-        return res
-          .status(500)
-          .json({ error: "Erreur lors de l'ajout de l'utilisateur" });
-      }
-      res
-        .status(201)
-        .json({
-          message: "Utilisateur ajouté avec succès",
-          id: result.insertId,
-        });
+    res.status(201).json({
+      message: "Utilisateur ajouté avec succès",
+      id: result.insertId,
     });
   });
 });
